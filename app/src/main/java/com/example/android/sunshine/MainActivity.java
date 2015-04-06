aa99aca31391f30b151c191af3a6ad501ef3d39e
Dave Barnum <dbarnum@enernoc.com>
2015-04-06 13:54:21 -0400

    sanity

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,8 +15,33 @@ public class MainActivity extends ActionBarActivity {
     private static String LOG_TAG = MainActivity.class.getName();
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(LOG_TAG,"--------------onStop-----------------");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(LOG_TAG,"--------------onPause-----------------");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(LOG_TAG,"--------------onStart-----------------");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(LOG_TAG,"--------------onDestroy-----------------");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(LOG_TAG,"--------------onCreate----------------");
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -24,7 +49,6 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
